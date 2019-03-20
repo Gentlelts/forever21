011d34df92654c66c2f40f71990aafe49e3b5728
tomlin <tom@mecourse.cn>
2019-03-20 22:27:11 +0800

    '登录注册逻辑'

--- forever21/src/app/pages/login/login.page.ts
+++ forever21/src/app/pages/login/login.page.ts
@@ -1,19 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import axios from "axios";
+declare var JSEncrypt:any;
 @Component({
   templateUrl: './login.page.html',
   
   styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-  constructor() { }
+  constructor(
+    public http: HttpClient,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
   userName = '';
   passWord = '';
   LoginIn(){
     let data = {
-      userName:this.userName,
-      passWord:this.passWord
+      UserName:this.userName,
+      PassWord:this.passWord
     };
-    console.log(data)
+    axios({
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'get',
+      url: '/loginRegister/encrypt',
+    }).then((response) => {
+      let publicPem = response.data.data;
+      console.log(publicPem);
+      let encrypt = new JSEncrypt();
+      encrypt.setPublicKey(publicPem);
+      data.PassWord = encrypt.encrypt(data.PassWord);
+      axios({
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        method: 'post',
+        url: '/loginRegister/login',
+        data: data,
+        transformRequest: [function(data) {
+          let ret = '';
+          for(let it in data) {
+            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+          }
+          return ret
+        }],
+      }).then((response) => {
+        console.log(response);
+        if(response.data.code == 200){
+          alert("登录成功！");
+          let UserID = response.data.data.UserID;
+          window.localStorage.setItem('UserID',UserID);
+          window.localStorage.setItem('UserName',this.userName);
+          window.localStorage.setItem('PassWord',this.passWord);
+        }
+        if(response.data.code == 402){
+          alert(response.data.msg);
+        }
+      })
+    })
   }
   ngOnInit() {
   }
