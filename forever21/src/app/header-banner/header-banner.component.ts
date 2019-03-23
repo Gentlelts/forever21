f64a100a2eaf18d6312b7e59dc7b06efe0cf61fa
tomlin <tom@mecourse.cn>
2019-03-23 19:42:00 +0800

    发布作品

--- forever21/src/app/header-banner/header-banner.component.ts
+++ forever21/src/app/header-banner/header-banner.component.ts
@@ -11,14 +11,14 @@ export class HeaderBannerComponent implements OnInit {
   constructor(
     private router: Router,
   ) { }
-  
+
+  hasLogin = false;
   LoginOut() {
     window.localStorage.removeItem('UserID');
     window.localStorage.removeItem('UserName');
     this.router.navigate(['/']);
     this.hasLogin = true;
   }
-  hasLogin = false;
   ngOnInit() {
     let UserID = window.localStorage.getItem('UserID');
     this.hasLogin = UserID != null;
