9d429ffdb1e9398b4a283062a24b4858003d1236
lts <gentlelts@163.com>
2019-03-26 14:33:56 +0800

    个人中心

--- forever21/src/app/pages/personal-center/personal-center.page.ts
+++ forever21/src/app/pages/personal-center/personal-center.page.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {foreverHttp} from '../../../environments/axiosHttp';
+import {environment} from '../../../environments/environment';
 @Component({
   templateUrl: './personal-center.page.html',
 
   styleUrls: ['./personal-center.page.scss']
 })
 export class PersonalCenterPage implements OnInit {
+  public apiUrl= `${environment.url}`;
   userName = '';
+  showArticleList = false;// 显示故事列表与上传故事
+  hiddenArticleList = false;// 显示上传故事
+  ownerArticleList:any = [];
   constructor(
     private router: Router
   ) {
     this.userName = window.localStorage.getItem('UserName');
+    this.getUserInfo();
+    this.getUserArticle();
+  }
+  // 获取用户信息
+  getUserInfo(){
+    let data = {
+      UserID:window.localStorage.getItem('UserID')
+    };
+    foreverHttp.post('/userInfo/information',data,(res:any) =>{
+      if(res.code === 200){
+      }
+    });
+  }
+  // 获取用户故事列表
+  getUserArticle(){
+    let data = {
+      UserID:window.localStorage.getItem('UserID')
+    };
+    foreverHttp.post('/userInfo/userArticle',data,(response:any) =>{
+      if(response.code === 200){
+        for (let i = 0; i < response.data.length; i++) {
+          response.data[i].DocUrl = response.data[i].DocUrl.split(',');
+          response.data[i].DocUrl = response.data[i].DocUrl[0];
+        }
+        this.ownerArticleList = response.data;
+        console.log(this.ownerArticleList);
+        if(response.data.length > 0){
+          this.showArticleList = true;
+        } else {
+          this.hiddenArticleList = true;
+        }
+      }
+    });
   }
   releaseStory(){
     this.router.navigate(['/releaseStory']);
