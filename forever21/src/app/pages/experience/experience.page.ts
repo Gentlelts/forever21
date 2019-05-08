b4e264253fedb54202dbfa5ce373b38e64448450
lts <gentlelts@163.com>
2019-05-08 18:57:18 +0800

    搜索

--- forever21/src/app/pages/experience/experience.page.ts
+++ forever21/src/app/pages/experience/experience.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import {foreverHttp} from '../../../environments/axiosHttp';
+
 @Component({
   templateUrl: './experience.page.html',
   styleUrls: ['./experience.page.scss']
@@ -10,6 +12,7 @@ export class ExperiencePage implements OnInit {
     private route: ActivatedRoute,
     ) { }
   public keyword: any;
+  public storyList: any;
   
   getKeyword() {
     this.route.queryParams.subscribe(params => {
@@ -17,7 +20,23 @@ export class ExperiencePage implements OnInit {
     console.log(this.keyword)
     })
   }
+  getStoryList(){
+    let data = {
+      tags:''
+    };
+    foreverHttp.get('/article/list',data,(response:any) =>{
+      if (response.code === 200) {
+        for (let i = 0; i < response.data.length; i++) {
+          response.data[i].DocUrl = response.data[i].DocUrl.split(',');
+          response.data[i].DocUrl = response.data[i].DocUrl[0];
+        }
+        this.storyList = response.data;
+        console.log(this.storyList)
+      }
+    });
+  }
   ngOnInit() {
     this.getKeyword();
+    this.getStoryList();
   }
 }
